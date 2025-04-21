8c55724fc90063fc22fcff9b716c70f6fe4fe02a
Devesh Chandra <deveshru2712@gmail.com>
2025-04-21 18:27:09 +0530

    conditionals

---
+++ conditional/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+
+    let a = 99;
+    // if a>99{
+    //     println!("big number");
+    // }else{
+    //     println!("small number");
+    // }
+
+    // if a>99{
+    //     if>a>200{
+    //         println!("huge number");
+    //     }else{
+    //         println!("big number");
+    //     }
+    // }else{
+    //     println!("small number");
+    // }
+
+    let a = 99;
+    if a>200{
+        println!("huge number")
+    }else if a>99{
+        println!("big number")
+    }else{
+        println!("small number")
+    }
+
+
+}
