bee7cf205d070879537eeae31082dd553ba5ea75
Devesh Chandra <deveshru2712@gmail.com>
2025-04-21 18:35:03 +0530

    loops

---
+++ loops/src/main.rs
@@ -0,0 +1,25 @@
+// loop -> infinite loop until the condition is not meant
+
+// while -> conditional loop until the condition is true
+
+
+// loop
+
+fn main (){
+    let mut a = 0;
+
+    // loop{
+    //     if a == 5{
+    //         break;
+    //     }
+    //     println!("{:?}",a);
+    //     a=a+1;
+    // }
+
+    while a !=5 {
+        println!("{:?}",a);
+        a=a+1;
+        
+    }
+}
+
